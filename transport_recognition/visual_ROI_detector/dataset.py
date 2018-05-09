da165d601c516b64f901966dfe3b3f19487caf4d
Nathan Lacroix <lna@student.ethz.ch>
2018-05-09 16:20:59 +0200

    dataset first implementation

--- transport_recognition/visual_ROI_detector/dataset.py
+++ transport_recognition/visual_ROI_detector/dataset.py
@@ -3,8 +3,8 @@ import os
 from object_detection.dataset_tools.create_coco_tf_record import _create_tf_record_from_coco_annotations
 
 annotations_file = '/home/nathan/Visually-Impaired-Transport-Assistance/data/' \
-                    'coco/annotations/instances_train2017.json'
-image_dir = '/home/nathan/Visually-Impaired-Transport-Assistance/data/coco/images'
+                    'coco/annotations/instances_val2017.json'
+image_dir = '/home/nathan/Visually-Impaired-Transport-Assistance/data/coco/images/val'
 output_dir = '/home/nathan/Visually-Impaired-Transport-Assistance/data/coco/tfrecord'
 
 def dataset2TFRecord(annotations_file, image_dir, output_dir,
@@ -18,6 +18,6 @@ def dataset2TFRecord(annotations_file, image_dir, output_dir,
                                             output_path,
                                             include_mask)
 
-dataset2TFRecord(annotations_file, image_dir, output_dir, 'train')
+dataset2TFRecord(annotations_file, image_dir, output_dir, 'val')
 
 
